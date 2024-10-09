52446d60f8cf03729137b64f3282bdc5648008e6
Quasant <benjaminthechin@gmail.com>
2024-10-09 20:26:39 +0800

    Modify DeleteJobCommand to automatically show list of job

--- src/main/java/seedu/address/logic/commands/DeleteJobCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteJobCommand.java
@@ -7,6 +7,7 @@ import java.util.List;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
+import seedu.address.logic.Mode;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.job.Job;
@@ -42,7 +43,8 @@ public class DeleteJobCommand extends Command {
 
         Job jobToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deleteJob(jobToDelete);
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(jobToDelete)));
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(jobToDelete)),
+                false,  false, Mode.JOB);
     }
 
     @Override
