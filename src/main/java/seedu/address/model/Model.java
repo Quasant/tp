e79f5347cd42af25f613a26735378eea8cec7389
Quasant <benjaminthechin@gmail.com>
2024-10-09 20:29:20 +0800

    Complete to-dos within the model interface

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -60,7 +60,6 @@ public interface Model {
     boolean hasPerson(Person person);
 
     /**
-     * Todo: Do we want to refer to the different lists as address book or split it (Contacts, Job board etc)
      * Returns true if a job with the same identity as {@code job} exists in the address book.
      */
     boolean hasJob(Job job);
@@ -72,7 +71,6 @@ public interface Model {
     void deletePerson(Person target);
 
     /**
-     * Todo: Implement in subsequent PRs
      * Deletes the given job.
      * The job must exist in the address book.
      */
